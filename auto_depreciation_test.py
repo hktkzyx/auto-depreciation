2e4095824bed90f05947bfffd6af909b395ea602
Ankur Dave <ankurdave@gmail.com>
2022-02-21 14:00:01 -0500

    Fix rounding error in depreciation value using decimal arithmetic

--- auto_depreciation_test.py
+++ auto_depreciation_test.py
@@ -61,12 +61,12 @@ class TestExampleAutoDepreciation(cmptest.TestCase):
         2020-04-30 * "Test-auto_depreciation:Nikon"
           Assets:Fixed-Assets   -2 LENS {600.00 CNY, 2020-03-31, "Nikon"}
           Assets:Fixed-Assets    2 LENS {380 CNY, 2020-04-30, "Nikon"}   
-          Expenses:Depreciation    440 CNY
+          Expenses:Depreciation    440.00 CNY
 
         2020-04-30 * "Test-auto_depreciation"
           Assets:Fixed-Assets            -1 LENS {800.00 CNY, 2020-03-31}
           Assets:Fixed-Assets             1 LENS {207 CNY, 2020-04-30}
-          Expenses:Depreciation  593 CNY
+          Expenses:Depreciation  593.00 CNY
 
         2020-05-31 * "Test-auto_depreciation:Nikon"
           Assets:Fixed-Assets   -2 LENS {380 CNY, 2020-04-30, "Nikon"}
@@ -82,6 +82,73 @@ class TestExampleAutoDepreciation(cmptest.TestCase):
           Assets:Fixed-Assets  -2 LENS {243 CNY, 2020-05-31, "Nikon"}
           Assets:Fixed-Assets   2 LENS {200 CNY, 2020-06-30, "Nikon"}
           Expenses:Depreciation    86 CNY
+
+        """
+        self.assertEqualEntries(expected_entries, entries)
+        self.assertFalse(errors)
+
+    def test_rounding_errors(self):
+        sample = """
+        option "insert_pythonpath" "True"
+        plugin "auto_depreciation" "{
+          'assets':'Assets:Fixed-Assets',
+          'expenses':'Expenses:Depreciation',
+        }"
+
+        2020-03-01 open Assets:Cash CNY
+        2020-03-01 open Assets:Fixed-Assets
+        2020-03-01 open Expenses:Depreciation
+        2020-03-01 open Equity:Opening-Balances
+
+        2020-03-01 commodity LENS
+            name: "Camera lens"
+            assets-class: "fixed assets"
+
+        2020-03-01 * ""
+            Assets:Cash                     2999.85 CNY
+            Equity:Opening-Balances
+
+        2020-03-31 * "Test"
+            Assets:Cash                     -2999.85 CNY
+            Assets:Fixed-Assets        3 LENS {999.95 CNY, "Nikon"}
+              useful_life: "3m"
+              residual_value: 200.05
+        """
+        entries, errors, _ = loader.load_string(textwrap.dedent(sample))
+        expected_entries = """
+        2020-03-01 open Assets:Cash                                     CNY
+        2020-03-01 open Assets:Fixed-Assets
+        2020-03-01 open Expenses:Depreciation
+        2020-03-01 open Equity:Opening-Balances
+
+        2020-03-01 commodity LENS
+          assets-class: "fixed assets"
+          name: "Camera lens"
+
+        2020-03-01 * 
+          Assets:Cash               2999.85 CNY
+          Equity:Opening-Balances  -2999.85 CNY
+
+        2020-03-31 * "Test"
+          Assets:Cash              -2999.85 CNY
+          Assets:Fixed-Assets        3 LENS {999.95 CNY, 2020-03-31, "Nikon"}
+            useful_life: "3m"
+            residual_value: 200.05
+
+        2020-04-30 * "Test-auto_depreciation:Nikon"
+          Assets:Fixed-Assets   -3 LENS {999.95 CNY, 2020-03-31, "Nikon"}
+          Assets:Fixed-Assets    3 LENS {559 CNY, 2020-04-30, "Nikon"}
+          Expenses:Depreciation    1322.85 CNY
+
+        2020-05-31 * "Test-auto_depreciation:Nikon"
+          Assets:Fixed-Assets   -3 LENS {559 CNY, 2020-04-30, "Nikon"}
+          Assets:Fixed-Assets    3 LENS {287 CNY, 2020-05-31, "Nikon"}
+          Expenses:Depreciation    816 CNY
+
+        2020-06-30 * "Test-auto_depreciation:Nikon"
+          Assets:Fixed-Assets  -3 LENS {287 CNY, 2020-05-31, "Nikon"}
+          Assets:Fixed-Assets   3 LENS {200 CNY, 2020-06-30, "Nikon"}
+          Expenses:Depreciation    261 CNY
 
         """
         self.assertEqualEntries(expected_entries, entries)
